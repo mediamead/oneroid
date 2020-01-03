280bf58e2382b252653007e42d19b982f20e1dae
abb@gremwell.com <abb@gremwell.com>
2020-01-03 12:38:51 +0300

    simple test run script

---
+++ eye_on_stick/main.py
@@ -0,0 +1,12 @@
+import gym
+env = gym.make('eye_on_stick:EyeOnStick-v0')
+while True:
+  env.reset()
+  info_shown = False
+  for _ in range(1000):
+    env.render()
+    (obs, reward, done, info) = env.step(env.action_space.sample()) # take a random action
+    if not info_shown:
+        print(info)
+        info_shown = True
+env.close()
