269322861aca60ae37791a44f44db4459edcbd59
Alexandre Bezroutchko <alexandre.bezroutchko@gmail.com>
2020-03-24 23:07:31 +0100

    lstm

--- train-run2.py
+++ train-run2.py
@@ -2,23 +2,22 @@
 
 import gym
 
-from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.policies import MlpPolicy, MlpLnLstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
+from stable_baselines.common.vec_env import SubprocVecEnv
 
-#env = gym.make('CartPole-v1')
-env = gym.make('eye_on_stick:EyeOnStick-v0')
-# Optional: PPO2 requires a vectorized environment to run
-# the env is now wrapped automatically when passing it to the constructor
-# env = DummyVecEnv([lambda: env])
+if __name__ == '__main__':
+    n_cpu = 16
+    env = SubprocVecEnv([lambda: gym.make('eye_on_stick:EyeOnStick-v0') for i in range(n_cpu)])
 
-model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./tensorboard/")
-model.learn(total_timesteps=10000)
+    model = PPO2(MlpLnLstmPolicy, env, nminibatches=16, verbose=1, tensorboard_log="./tensorboard/")
+    model.learn(total_timesteps=10000)
 
-while True:
-    obs = env.reset() #keep_phi=True)
-    done = False
-    while not done:
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        env.render()
+    while True:
+        obs = env.env_method('reset') #keep_phi=True)
+        done = [False]
+        while not all(done):
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
+            env.env_method('render', indices=[0])
