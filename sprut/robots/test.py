67d11c71caff09a6bcf154e4e237c9b3a152d55d
Alexandre Bezroutchko <alexandre.bezroutchko@gmail.com>
2020-05-30 18:17:03 +0200

    disable camorn

--- sprut/robots/test.py
+++ sprut/robots/test.py
@@ -7,7 +7,7 @@ from pybullet_robot import PyBulletRobot, W, H
 
 import cv2
 
-from camorn import set_headcam_params, get_horizon_bank
+#from camorn import set_headcam_params, get_horizon_bank
 
 if __name__ == "__main__":
     #LOGFILE = "log10s.txt"
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     gui = True
     r = PyBulletRobot(4, 4, render=gui)
 
-    set_headcam_params(W, H)
+    #set_headcam_params(W, H)
 
     # Target and manipulator are both NNE
     r.setTarget([1.5, 1.5, 1])
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         #print("cam_p %s, cam_v %s" % (cam_p, cam_v))
 
         img = r.getCameraImage()
-        print("horizon_bank=%s" % get_horizon_bank(img))
+        #print("horizon_bank=%s" % get_horizon_bank(img))
 
         #print("oc=%s, qval=%f, done=%s" % ((r.dx, r.dy), r.qval, r.done))
 
