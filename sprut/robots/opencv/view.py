ecb87fd88141cc36b07b8ddfc399e44f94806aad
Alexandre Bezroutchko <alexandre.bezroutchko@gmail.com>
2020-06-02 18:16:08 +0200

    view.py: save captured images

--- sprut/robots/opencv/view.py
+++ sprut/robots/opencv/view.py
@@ -1,16 +1,28 @@
 import cv2
+import time
 
-# Set resolution
+# Get resolution
 cap = cv2.VideoCapture(1)
 print("Default resolution: %sx%s" % (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
+# Set resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 print("New resolution: %sx%s" % (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
+def save(img):
+    filename = "img-%d.jpg" % int(time.time() * 1e6)
+    cv2.imwrite(filename, img)
+    print("captured image saved in '%s'" % filename)
+
 while True:
-    ret, img = cap.read()
-    assert(ret)
+    retval, img = cap.read()
+    assert(retval)
+
     cv2.imshow('img', img)
-    cv2.waitKey(100)
+
+    k = cv2.waitKey(1) & 0xff
+    if k == ord('q'):
+        break
+    elif k == ord('s'):
+        save(img)
