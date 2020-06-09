1d54d43f37b697292e7008acb36056870bbabddd
unknown <abb@gremwell.com>
2020-06-09 05:34:24 +0300

    view.py: support custom resolution

--- opencv/view.py
+++ opencv/view.py
@@ -9,8 +9,12 @@ from opencv_tools import save, resize
 print("### Press 's' to save the frame, 'q' to quit")
 
 cam_n = int(sys.argv[1])
-#cam = Camera(cam_n, 1280, 720)
-cam = Camera(cam_n, 1920, 1080)
+
+if len(sys.argv) > 2:
+    W, H = int(sys.argv[2]), int(sys.argv[3])
+else:
+    W, H = 1920, 1080
+cam = Camera(cam_n, W, H)
 
 while True:
     img = cam.read()
